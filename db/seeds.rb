2c9cc20e3c9c0ff91c87abd7d73e9caffafedd19
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2023-01-15 11:59:46 +0300

    Seeded dares

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ 
+puts "Seeding dares "
+
+dares = Dare.create([
+    {body:"Kiss the buttcheek of the person to your left (or get someone to kiss yours)"},
+    {body:"Let the person to your right sit on your back and do 5 pushups"},
+    {body:"Call a pizza chain and ask for items by a competitor brand"}    ,
+    {body:"Call a chicken takeout chain and ask for kienyeji chicken"},
+    {body:"Eat a banana without removing the skin"},
+    {body:"Sing to a song of your choice , record and post it"},
+    {body:"moan the name of the person to your right"},
+    {body:"Demonstrate your favourite sex style with a team player of your choice"},
+    {body:"Call your ex and tell them you really miss them"},
+    {body:"Shave your eyebrow"},
+    {body:"Take an embarrasing snap and post it on all your socials"},
+    {body:"Propose to a team player , record and post it"},
+    {body:"spin the bottle and motorboat the person it points to"},
+    {body:"Choose a team member to have 7 minutes in heaven with"},
+    {body:"Post I really miss my ex on all your socials"},
+    {body:"Pick a cuffed player and text the partner (He/She just cheated)"},
+    {body:"Spin the bottle and twerk on who it points to"},
+    {body:"Spin the bottle and kiss whoever it points to"},
+    {body:"Call your crush and tell them you really miss them"},
+    {body:"Let the whole group go through your gallery"},
+    {body:"Text your current ex's current partner He/She is Still mine)"},
+    {body:"Record and post a video of you dancing"},
+    {body:"Pick and kiss a member of the group"},
+    {body:"Call yur guardian and tell them you are really drunk"},
+    {body:"Let the group choose someone you should send a risky text to"},
+    {body:"Pick and take/give a member a lapdance"},
+    {body:"Let the player on your right go through your snap for your eyes only"},
+    {body:"Show your orgasm face"},
+    {body:"Show the most embarrasing picture in your gallery"},
+    {body:"The players on your right and left must kiss for a minute"},
+    {body:"Post a picture pretending to drink from toilet water with the caption (so thirsty)"},
+    {body:"Do your best impression of a stripper"},
+    {body:"Call  a random person and flirt with the person who picks up"},
+    {body:"Send a voicenote to your crush confessing your feelings"},
+    {body:"Text (I am cumming) twice on your status the (I just came)"},
+    {body:"Let the group go through your Instagram DMS"},
+    {body:"Send a sext to the last number of the opposite gender in your phonebook"},
+    {body:"Pretend to be the person on your right for 2  minutes"},
+    {body:"Remove a piece of clothing"},
+    {body:"Down the drink in front of you"},
+    {body:"Show your google search history"},
+    {body:"Let the group slide in someones DM on your instagram"},
+    {body:"Empty your wallet or purse and let everyone go through what is inside"},
+    {body:"Be blindfolded and let the group feed you something"},
+    {body:"Go outside and sing aloud for a minute"},
+    {body:"Try and lick your elbow and record and post it "},
+    {body:"Call a random number and sing the national anthem to them"},
+    {body:"Let the person across you give you a hickey"},
+    {body:"Comment on the first 5 posts on your FYP with (so hot) "},
+    {body:"make out with your hand for a minute"},
+    {body:"Post on your socials (i am so horny if you are free hmu)"},
+    {body:"Call your guardian and telll them you just slept with someone"},
+
+
+])
+    
+puts "Finished seeding dares"
